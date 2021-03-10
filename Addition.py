330e5972d14273f18db76ad882dbaf37343d7aab
Pakshal Shashikant Jain <80408249+PakshalShahsikantJain@users.noreply.github.com>
2021-03-10 21:49:35 +0530

    Second Code

---
+++ Addition.py
@@ -0,0 +1,11 @@
+print("Jay Ganesh.......")
+
+print("Enter First Number")
+No = input()
+
+print("Enter Second Number")
+No1 = input()
+
+Sum = int(No) + int(No1)
+
+print("Addition of Two Number is :",Sum)
