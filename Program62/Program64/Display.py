8456a3e1022286d5419b11111f2bd60dcd571e0b
Pakshal Shashikant Jain <80408249+PakshalShahsikantJain@users.noreply.github.com>
2021-03-31 12:52:06 +0530

    Delete Program62 directory

--- Program62/Program64/Display.py
+++
@@ -1,9 +0,0 @@
-def Display(No) :
-    i = 1
-    
-    if No < 0 :
-        No = -No
-
-    while i <= No :
-        print(i," ","*"," ",end = " ")
-        i = i + 1
--- Program62/Program76/Pattern.py
+++
@@ -1,9 +0,0 @@
-def Display(Row,Col,No) :
-    i = 0
-    j = 0
-
-    for i in range(Row,0,-1) :
-        for j in range(1,Col + 1) :
-            print(No," ",end = " ")
-            No = No + 1
-        print("\r")
