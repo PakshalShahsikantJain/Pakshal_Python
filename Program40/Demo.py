00a0711f56502147c6d67362c90a8e89ac58c5ad
Pakshal Jain <pakshal1256@gmail.com>
2021-06-24 20:25:53 +0530

    Update Demo.py

--- Program40/Demo.py
+++ Program40/Demo.py
@@ -1,3 +1,11 @@
+#####################################################################################
+#
+# Author : Pakshal Shahsikant Jain
+# Problem_Statement : Display Table of Entered Number 
+# Date : 30/02/2021 
+#
+######################################################################################
+
 from Mult import *
 
 def main() :
