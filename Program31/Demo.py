31bbe518fc6dbec79ae85f092076995974337d96
Pakshal Jain <pakshal1256@gmail.com>
2021-06-24 19:55:25 +0530

    Update Demo.py

--- Program31/Demo.py
+++ Program31/Demo.py
@@ -1,3 +1,12 @@
+#####################################################################################
+#
+# Author : Pakshal Shahsikant Jain
+# Problem_Statement : Display Count of Digits Less Than From Entered Number 
+# Example : Input = 40262 Output = Frequecy(Count) of Digits Less Than 6 is : 4  
+# Date : 30/02/2021 
+#
+######################################################################################
+
 from Digits import *
 
 def  main() :
