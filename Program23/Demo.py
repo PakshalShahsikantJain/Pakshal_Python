27a91b6ae61fea633c17bfbf4e59ceb3d5be8831
Pakshal Jain <pakshal1256@gmail.com>
2021-06-24 19:53:15 +0530

    Update Demo.py

--- Program23/Demo.py
+++ Program23/Demo.py
@@ -1,3 +1,11 @@
+#####################################################################################
+#
+# Author : Pakshal Shahsikant Jain
+# Problem_Statement : Check Whether Entered Number is Greater Than Hundred or Not 
+# Date : 30/02/2021 
+#
+######################################################################################
+
 from Check import * 
 
 def main() :
