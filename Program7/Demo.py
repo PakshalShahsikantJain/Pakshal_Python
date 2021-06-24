ac15e6d8b9af9177d244f95ef9cc2c26871bc7ae
Pakshal Jain <pakshal1256@gmail.com>
2021-06-24 16:31:15 +0530

    Update Demo.py

--- Program7/Demo.py
+++ Program7/Demo.py
@@ -1,3 +1,11 @@
+#####################################################################################
+#
+# Author : Pakshal Shahsikant Jain
+# Problem_Statement : Display Stars Number of Times Entered By User 
+# Date : 30/02/2021 
+#
+######################################################################################
+
 print("Jay Ganesh.....")
 
 print("Enter Number to Print Stars")
