76f5c5aa47458cc63756bbd10a2b6fe120e8de53
Pakshal Jain <pakshal1256@gmail.com>
2021-06-24 19:55:12 +0530

    Update Demo.py

--- Program29/Demo.py
+++ Program29/Demo.py
@@ -1,3 +1,12 @@
+#####################################################################################
+#
+# Author : Pakshal Shahsikant Jain
+# Problem_Statement : Display Count of 2 From Entered Number 
+# Example : Input = 40262 Output = Frequecy(Count) of 2 is : 2  
+# Date : 30/02/2021 
+#
+######################################################################################
+
 from Digits import *
 
 def  main() :
