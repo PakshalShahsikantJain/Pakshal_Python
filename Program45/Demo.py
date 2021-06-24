a1039c1dae5efddae7e001736850e1955a7067de
Pakshal Jain <pakshal1256@gmail.com>
2021-06-24 20:25:59 +0530

    Update Demo.py

--- Program45/Demo.py
+++ Program45/Demo.py
@@ -1,3 +1,11 @@
+#####################################################################################
+#
+# Author : Pakshal Shahsikant Jain
+# Problem_Statement : Display Only Numbers Till Entered Number 
+# Date : 30/02/2021 
+#
+######################################################################################
+
 from Display import *
 
 def main() :
